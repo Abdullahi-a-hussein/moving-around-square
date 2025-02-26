a384fad577d71b253850d2be141e7faa152d3649
Abdullahi Hussein <abdullahi.a.hussein@gmail.com>
2025-02-26 14:16:05 -0500

    update starting position in each cirlce

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const canvas = document.getElementById("canvas");
-const width = 600;
-const height = 600;
+const width = 700;
+const height = 700;
 
 canvas.width = width;
 canvas.height = height;
 
 const ctx = canvas.getContext("2d");
 let squares = [];
-const totalSquares = 40;
+const totalSquares = 80;
 const squareSize = 10;
 const gap = 5;
 
@@ -24,7 +24,6 @@ function drawSquares() {
       const y = j * (squareSize + gap);
       const square = new Square(x, y, squareSize);
       newSquares.push(square);
-      square.draw(ctx);
     }
   }
   return newSquares;
@@ -35,73 +34,89 @@ class Square {
     this.x = x;
     this.y = y;
     this.size = size;
+    this.t = Math.random();
+    this.j = Math.random();
+    this.round = Math.random() > 0.5 ? 1 : 0;
+    this.increment = 0.05;
   }
   draw(ctx) {
     ctx.beginPath();
     ctx.rect(this.x, this.y, this.size, this.size);
     ctx.stroke();
   }
-  drawPont(ctx, t, j, round) {
+  drawPont(ctx) {
     let point;
-    if (t > 0 && round === 0) {
+    if (this.t > 0 && this.round === 0) {
       point = {
-        x: lerp(this.x, this.x + this.size, t),
+        x: lerp(this.x, this.x + this.size, this.t),
         y: this.y,
       };
-    } else if (t > 0 && round == 1) {
+    } else if (this.t > 0 && this.round == 1) {
       point = {
-        x: lerp(this.x + this.size, this.x, t),
+        x: lerp(this.x + this.size, this.x, this.t),
         y: this.y + this.size,
       };
-    } else if (t <= 0 && round == 0) {
+    } else if (this.t <= 0 && this.round == 0) {
       point = {
         x: this.x,
-        y: lerp(this.y + this.size, this.y, j),
+        y: lerp(this.y + this.size, this.y, this.j),
       };
-    } else if (t <= 0 && round == 1) {
+    } else if (this.t <= 0 && this.round == 1) {
       point = {
         x: this.x + this.size,
-        y: lerp(this.y, this.y + this.size, j),
+        y: lerp(this.y, this.y + this.size, this.j),
       };
     }
     ctx.beginPath();
-    ctx.arc(point.x, point.y, 2.5, 0, Math.PI * 2);
+    ctx.arc(point.x, point.y, 2, 0, Math.PI * 2);
     ctx.fill();
   }
-}
-let j = 1;
-let t = 1;
-let round = 0;
-let increment = 0.1;
-
-function animate() {
-  ctx.clearRect(0, 0, width, height);
-  squares = drawSquares();
-  if (round == 0) {
-    if (t > 0 && j == 1) {
-      t -= increment;
-    } else if (t <= 0 && j > 0) {
-      j -= increment;
-    } else if (t <= 0 && j <= 0) {
-      j = 1;
-      t = 1;
-      round = 1;
-    }
-  } else if (round == 1) {
-    if (t > 0 && j == 1) {
-      t -= increment;
-    } else if (t <= 0 && j > 0) {
-      j -= increment;
-    } else if (t <= 0 && j <= 0) {
-      j = 1;
-      t = 1;
-      round = 0;
+  update() {
+    if (this.round == 0) {
+      if (this.t > 0) {
+        this.t -= this.increment;
+      } else if (this.j > 0) {
+        this.j -= this.increment;
+      } else {
+        this.j = 1;
+        this.t = 1;
+        this.round = 1;
+      }
+    } else {
+      if (this.t > 0) {
+        this.t -= this.increment;
+      } else if (this.j > 0) {
+        this.j -= this.increment;
+      } else {
+        this.j = 1;
+        this.t = 1;
+        this.round = 0;
+      }
     }
   }
+}
+
+squares = drawSquares();
+const ln = squares.length;
+squares.forEach((square, index) => {
+  if (index % 2 == 0) {
+    square.t = Math.abs(Math.cos(index));
+    square.j = Math.abs(Math.cos(index));
+  } else {
+    square.t = Math.abs(Math.sin(ln - index));
+    square.j = Math.abs(Math.sin(ln - index));
+  }
+});
+function animate() {
+  ctx.clearRect(0, 0, width, height);
+
   ctx.fillStyle = "white";
   squares.forEach((square, index) => {
-    square.drawPont(ctx, t, j, round);
+    square.draw(ctx);
+    square.drawPont(ctx);
   });
+
+  squares.forEach((square) => square.update());
   requestAnimationFrame(animate);
 }
 
